e92187ad709c186c27ddef023c98469a3249a1c3
ayakkk <86222124+ayakkk@users.noreply.github.com>
2023-07-12 10:40:54 +0900

    new ex

---
+++ langchain_ex/constants.py
@@ -0,0 +1,1 @@
+openai_key='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'
--- langchain_ex/main.py
+++ langchain_ex/main.py
@@ -5,6 +5,7 @@ from constants import openai_key
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
 from langchain.chains import LLMChain
+from langchain.chains import SequentialChain
 import streamlit as st
 
 os.environ["OPENAI_API_KEY"]=openai_key
@@ -13,15 +14,30 @@ os.environ["OPENAI_API_KEY"]=openai_key
 st.title("OpenAI Langchain Playground")
 input_text=st.text_input("Search") 
 
-#promp template
+#prompt template
 first_input_prompt = PromptTemplate (
     input_variables=['firstvar'],
-    template= "Tell me about {firstvar}"
+    template= "Tell me about celebrity {firstvar}"
 )
 
 #initialize
 llm= OpenAI(temperature=0.8) #play w temperature
-chain=LLMChain(llm=llm, prompt=first_input_prompt, verbose=True)
+chain=LLMChain(llm=llm, prompt=first_input_prompt, verbose=True, output_key='person')
+
+#prompt template
+second_input_prompt = PromptTemplate (
+    input_variables=['person'],
+    template= "When was {person} born"
+)
+
+#initialize chain 2
+chain2=LLMChain(llm=llm, prompt=second_input_prompt, verbose=True, output_key='dob')
+
+parent_chain = SequentialChain(chains=[chain, chain2], input_variables=['firstvar'], output_variables=['person', 'dob'], verbose=True)
+
+
+#to add memory, add another var
+
 
 if input_text:
-    st.write(chain.run(input_text))
+    st.write(parent_chain.run({'name': input_text}))
