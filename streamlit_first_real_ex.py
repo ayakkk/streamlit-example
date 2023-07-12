0af183f6aa96119d1919cde8660f6126f4b6f16a
ayakkk <86222124+ayakkk@users.noreply.github.com>
2023-07-12 14:20:29 +0900

    yay??

--- streamlit_first_real_ex.py
+++ streamlit_first_real_ex.py
@@ -1,3 +1,34 @@
 import streamlit as st
+import os
 
-st.write("translator")
+'''
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain 
+'''
+#from langchain.chains import SequentialChain
+
+st.title("translator")
+input_text=st.text_input("Search") 
+
+
+####### set up  
+
+#os.environ["OPENAI_API_KEY"] = ''
+
+#template = '''Translate '{sentence}' into {target_language} '''
+
+#language_prompt = PromptTemplate(
+ #   input_variables=["sentence", 'target_language'],
+  #  template=template
+#)
+
+
+#language_prompt.format(sentence="this is a test", target_language='japanese')
+
+#chain 
+#llm = OpenAI(temperature=0.7)
+#chain2 = LLMChain(llm=llm, prompt=language_prompt)
+#if more than 1 parameter, need to use keys
+#chain2({'sentence': "Hello", 'target_language':'Japanese'})
+
