59f5129d0b3274f15852d476112432a1739ccefe
ayakkk <86222124+ayakkk@users.noreply.github.com>
2023-07-12 14:00:41 +0900

    what

---
+++ embeddings/main.py
@@ -0,0 +1,5 @@
+import os
+os.environ["OPENAI_API_KEY"] = ''
+
+from langchain.embeddings import OpenAIEmbeddings
+
---
+++ spotify/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+#possibly chatbot, pick 5 songs to plot type of thing
+
+#get personal play count
+
+#get world play count
+
+#plot
