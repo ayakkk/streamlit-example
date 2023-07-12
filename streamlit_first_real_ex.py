5399d1e80e278982593fd53801f48bd2900b9749
ayakkk <86222124+ayakkk@users.noreply.github.com>
2023-07-12 14:39:42 +0900

    config file

--- streamlit_first_real_ex.py
+++ streamlit_first_real_ex.py
@@ -1,34 +1,38 @@
 import streamlit as st
 import os
 
-'''
+
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
-from langchain.chains import LLMChain 
-'''
-#from langchain.chains import SequentialChain
+from langchain.chains import LLMChain  
+
+##from langchain.chains import SequentialChain
 
 st.title("translator")
 input_text=st.text_input(":fried_egg:") 
 
 
+
+
 ####### set up  
 
-#os.environ["OPENAI_API_KEY"] = ''
+# os.environ["OPENAI_API_KEY"] = ''
 
-#template = '''Translate '{sentence}' into {target_language} '''
+# template = '''Translate '{sentence}' into {target_language} '''
 
-#language_prompt = PromptTemplate(
- #   input_variables=["sentence", 'target_language'],
-  #  template=template
-#)
+# language_prompt = PromptTemplate(
+#    input_variables=["sentence", 'target_language'],
+#    template=template
+# )
 
 
 #language_prompt.format(sentence="this is a test", target_language='japanese')
 
-#chain 
-#llm = OpenAI(temperature=0.7)
-#chain2 = LLMChain(llm=llm, prompt=language_prompt)
-#if more than 1 parameter, need to use keys
-#chain2({'sentence': "Hello", 'target_language':'Japanese'})
+######chain 
+# llm = OpenAI(temperature=0.7)
+# chain2 = LLMChain(llm=llm, prompt=language_prompt)
+# #if more than 1 parameter, need to use keys
+# chain2({'sentence': "Hello", 'target_language':'Japanese'})
 
+
+#verified until openAI
