cb457f2a50285de63ba467e9863426ff7187dcd1
ayakkk <86222124+ayakkk@users.noreply.github.com>
2023-07-14 10:34:28 +0900

    key

--- streamlit_first_real_ex.py
+++ streamlit_first_real_ex.py
@@ -3,16 +3,16 @@ import os
 
 #F8F4F9
 
-#from langchain.llms import OpenAI
-#from langchain import PromptTemplate
-#from langchain.chains import LLMChain  
+from langchain.llms import OpenAI
+from langchain import PromptTemplate
+from langchain.chains import LLMChain  
 
 ##from langchain.chains import SequentialChain
 
 st.title("translator")
 
 selected_language = st.radio(
-    "Pick a language to translate into",
+    "Select a target language",
     ('English', 'French', 'Mandarin')
 )
     
@@ -20,28 +20,30 @@ input_text=st.text_input(":fried_egg:")
 
 
 
-
 ####### set up  
 
 # os.environ["OPENAI_API_KEY"] = ''
+os.environ["OPENAI_API_KEY"] = '809886fd96fd414a9ab72499e6486f8f'
 
-# template = '''Translate '{sentence}' into {target_language} '''
+template = '''Translate '{sentence}' into {target_language} '''
 
-# language_prompt = PromptTemplate(
-#    input_variables=["sentence", 'target_language'],
-#    template=template
-# )
+language_prompt = PromptTemplate(
+   input_variables=["sentence", 'target_language'],
+   template=template
+)
 
 
-#language_prompt.format(sentence="this is a test", target_language='japanese')
-#language_prompt.format(sentence="this is a test", target_language=selected_language)
+language_prompt.format(sentence="this is a test", target_language='japanese')
+language_prompt.format(sentence="this is a test", target_language=selected_language)
 
 ######chain 
-# llm = OpenAI(temperature=0.7)
-# chain2 = LLMChain(llm=llm, prompt=language_prompt)
-# #if more than 1 parameter, need to use keys
-# chain2({'sentence': "Hello", 'target_language':'Japanese'})
+llm = OpenAI(temperature=0.7)
+chain2 = LLMChain(llm=llm, prompt=language_prompt)
+#if more than 1 parameter, need to use keys
+chain2({'sentence': "Hello", 'target_language':'Japanese'})
 
 
 #verified until openAI
 #further implementation idea: allow multiple languages selection
+
+#take translated language and plug into deepL back to original language -> see whether it goes back to the original
